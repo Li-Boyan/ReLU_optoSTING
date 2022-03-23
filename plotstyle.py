31e9d125f0e598d299c82c15fdd7c08349a6083d
Boyan <84401016+Li-Boyan@users.noreply.github.com>
2022-03-23 16:56:01 +0800

    Add files via upload

---
+++ plotstyle.py
@@ -0,0 +1,25 @@
+from matplotlib import pyplot as plt
+import fire
+import os
+
+FIGURE_PATH = "../figures"
+
+def defaultStyle(fs=14):
+    plt.rc("font", family="Arial")
+    plt.rc("text", usetex=False)
+    plt.rc("xtick", labelsize=fs)
+    plt.rc("ytick", labelsize=fs)
+    plt.rc("axes", labelsize=fs)
+    plt.rc("mathtext", fontset="custom", rm="Arial")
+
+
+def save_fig(fig_id, tight_layout=True, fmt="pdf"):
+    path = os.path.join(FIGURE_PATH, fig_id + ".pdf")
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fmt)
+
+
+if __name__ == "__main__":
+    fire.Fire()
